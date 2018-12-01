902261694bb5a4419b479e032746dd0c62a6b3e0
hackedwifi <ajdelvalle@gmail.com>
2018-11-30 22:51:19 -0800

    add menu button rotate

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
 //select DOM items
 
-const menuBtn = document.querySelector(".btn-menu");
+const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
@@ -12,15 +12,14 @@ let showMenu = false;
 menuBtn.addEventListener("click", toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
-    menuBth.classList.add("close");
+    menuBtn.classList.add("close");
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
     showMenu = true;
   } else {
-    showMenu = true;
-    menuBth.classList.remove("close");
+    menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
